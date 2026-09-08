da6e32adf57e0c9b65f7629a28a3583d962ec806
Daniel Etim <detimworks@gmail.com>
2026-09-08 20:39:41 +0100

    Ready for deployment

--- blogger/settings.py
+++ blogger/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t__3ul@j9sk=s8_vxmor(l9jzcd&nkr8yrr2sxsl!na#v$peey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
